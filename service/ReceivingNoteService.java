667421efb0b754eba9da28e2b6a4d04d44af713f
s3819293 <s3819293@rmit.edu.vn>
2021-05-10 17:13:29 +0700

    Create Service

---
+++ service/ReceivingNoteService.java
@@ -0,0 +1,4 @@
+package com.example.sadi_assignment2_s3819293.service;
+
+public class ReceivingNoteService {
+}
---
+++ service/StaffService.java
@@ -0,0 +1,5 @@
+package com.example.sadi_assignment2_s3819293.service;
+
+public class StaffService {
+    
+}
