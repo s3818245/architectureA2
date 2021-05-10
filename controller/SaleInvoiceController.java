901a571d0999ade43ec1aff88531420f6e8252ef
s3819293 <s3819293@rmit.edu.vn>
2021-05-10 17:58:01 +0700

    running out of time

--- controller/SaleInvoiceController.java
+++ controller/SaleInvoiceController.java
@@ -1,14 +1,21 @@
 package com.example.sadi_assignment2_s3819293.controller;
 
+import com.example.sadi_assignment2_s3819293.model.SaleInvoice;
 import com.example.sadi_assignment2_s3819293.service.SaleInvoiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SaleInvoiceController {
     @Autowired
     private SaleInvoiceService saleInvoiceService;
 
-    @RequestMapping(path = "")
+    @RequestMapping(path = "/saleInvoices", method = RequestMethod.GET)
+    public List<SaleInvoice> getAllSaleInvoice() {
+        return saleInvoiceService.getAllSaleInvoice();
+    }
 }
--- controller/StaffController.java
+++ controller/StaffController.java
@@ -1,11 +1,22 @@
 package com.example.sadi_assignment2_s3819293.controller;
 
+import com.example.sadi_assignment2_s3819293.model.Order;
+import com.example.sadi_assignment2_s3819293.model.Staff;
 import com.example.sadi_assignment2_s3819293.service.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StaffController {
     @Autowired
     private StaffService staffService;
+
+    @RequestMapping(path = "/staffs", method = RequestMethod.GET)
+    public List<Staff> getAllStaff() {
+        return staffService.getAllStaff();
+    }
 }
