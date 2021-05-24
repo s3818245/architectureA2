359b0bdd5a15434f77f65200ee894610f93992f6
s3818245 <s3818245@rmit.edu.vn>
2021-05-24 17:01:04 +0700

    fix assumption

--- service/InventoryService.java
+++ service/InventoryService.java
@@ -28,36 +28,59 @@ public class InventoryService {
 
     public List<String> getInventory(Date start, Date end) {
 
-        //steps in the query
-        //- join receiving note table with receiving details
-        //- join delivery note table with delivery details
-        //- filter out the receiving note after start date and delivery note before start date
-        //- join product table with receiving details and delivery details => only products in both tables would be in the 
-        //query result
-        
-        SQLQuery queryForInventory = sessionFactory.getCurrentSession().createSQLQuery(
-                "select P.id, P.name, sum(R.quantity) as receivied , sum(D.quantity) as delivered, " +
-                        "sum(R.quantity) - sum(D.quantity) as balance " +
-                    "from product P, receivingDetail R, deliveryDetail D, receivingNote RN, deliveryNote DN " +
-                    "where R.receivingnote_receiving_note_id = RN.receiving_note_id and P.id = R.product_id " +
-                        "and P.id = D.product_id and D.deliverynote_delivery_note_id = DN.delivery_note_id " +
-                        "and RN.date >= :startdate and DN.date <= :enddate " +
+        //join product, receiving note and the receiving details table
+        //get the total of quantity of each product on the database
+        SQLQuery queryForReceiving = sessionFactory.getCurrentSession().createSQLQuery(
+                "select P.id, P.name, sum(R.quantity) as receivied "+
+                        "from product P, receivingDetail R, receivingNote RN " +
+                        "where R.receivingnote_receiving_note_id = RN.receiving_note_id and P.id = R.product_id " +
+                        "and RN.date >= :startdate and RN.date <= :enddate " +
                         "group by P.id;"
         );
 
-        queryForInventory.setParameter("startdate", start);
-        queryForInventory.setParameter("enddate", end);
+        //get the total of quantity of each product on the database
+        //join product, delivering note and the delivering details table
+        SQLQuery queryForDelivered = sessionFactory.getCurrentSession().createSQLQuery(
+                "select P.id, P.name, sum(D.quantity) as delivered "+
+                        "from product P, deliveryDetail D, deliveryNote DN " +
+                        "where P.id = D.product_id and D.deliverynote_delivery_note_id = DN.delivery_note_id " +
+                        "and DN.date >= :startdate and DN.date <= :enddate " +
+                        "group by P.id;"
+        );
 
-        //change format of each result to array
-        List<Object[]> result = queryForInventory.list();
+        //set the parameter in the query to start and end date specified 
+        queryForDelivered.setParameter("startdate", start);
+        queryForDelivered.setParameter("enddate", end);
+        queryForReceiving.setParameter("startdate", start);
+        queryForReceiving.setParameter("enddate", end);
+        
+        //convert query result to list
+        List<Object[]> delivered = queryForDelivered.list();
+        List<Object[]> received = queryForReceiving.list();
 
         //List that will store server response
         List<String> response = new ArrayList<>();
 
-        for (Object[] line : result){
-            String newLineInResponse = "Product: " + line[1].toString() + " (id - " + line[0].toString()
-                                        + ") | Received: " + line[2].toString() + " | Delivered: " + line[3].toString() +
-                                        " | Balance: " + line[4].toString();
+        for (Object[] receivedProducts : received){//go through each result of the receiving query
+            boolean productDelivered = false;
+            String newLineInResponse = "";
+
+            for (Object[] deliveredProducts : delivered){//go through each result of the delivering query
+                if (receivedProducts[0] == deliveredProducts[0]){//if product is delivered
+                    // in the response set balance to received - delivered
+                     newLineInResponse = "Product: " + receivedProducts[1].toString() + " (id - " + receivedProducts[0].toString()
+                            + ") | Received: " + receivedProducts[2].toString() + " | Delivered: " + deliveredProducts[2].toString() +
+                            " | Balance: " + ( Integer.parseInt(receivedProducts[2].toString()) - Integer.parseInt(deliveredProducts[2].toString()));
+                    productDelivered = true;
+                    break;//break inner for loop
+                }
+            }
+
+            if (!productDelivered){ // if the product is not delivered, set the balance to the amount received
+                newLineInResponse = "Product: " + receivedProducts[1].toString() + " (id - " + receivedProducts[0].toString()
+                        + ") | Received: " + receivedProducts[2].toString() + " | Delivered: 0"+
+                        " | Balance: " + receivedProducts[2].toString();
+            }
 
             response.add(newLineInResponse);
         }
